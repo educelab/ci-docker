4096b336250db67b9e9d117a586c309b1a54538c
Seth Parker <csparker247@gmail.com>
2018-05-01 16:52:48 -0400

    Separate into Dynamic and Static images.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+REPO=volcart/vcbuilder-debian
+VERSION=9_v4
+
+docker build -t ${REPO}:${VERSION}.base -f Dockerfile.base . && \
+docker build -t ${REPO}:${VERSION}.static -f Dockerfile.static . && \
+docker build -t ${REPO}:${VERSION}.dynamic -f Dockerfile.dynamic .
