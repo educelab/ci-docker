3c258b319a6ef8c83ba8c37c39d41caa601b9aa3
Seth Parker <csparker247@gmail.com>
2023-12-15 09:09:50 -0500

    Disable gh workflow. Simplify tags

--- build.sh
+++ build.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
+set -e
+
+timestamp() {
+    date -u +'%Y-%m-%dT%H:%M:%SZ'
+}
+
 REPO=ghcr.io/educelab/ci-docker
-VERSION=12_v1
+MAJOR=12
+MINOR=0
+PATCH=0
+SUFFIX=
+VERSION_FULL=${MAJOR}.${MINOR}.${PATCH}${SUFFIX}
+REV=$(git rev-parse --verify HEAD)
 
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ${REPO}:${VERSION}.base -f Dockerfile.base . && \
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ${REPO}:${VERSION}.dynamic -f Dockerfile.dynamic . && \
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ${REPO}:latest -t ${REPO}:${VERSION}.static -f Dockerfile.static .
+labels() {
+    echo --label org.opencontainers.image.created=$(timestamp) \
+    --label org.opencontainers.image.licenses=AGPL-3.0 \
+    --label org.opencontainers.image.revision=${REV} \
+    --label org.opencontainers.image.url=https://github.com/educelab/ci-docker \
+    --label org.opencontainers.image.version=${VERSION_FULL}
+}
 
-# Extra tags
-docker buildx build --platform linux/amd64 --push -t ${REPO}:${VERSION}.base.amd64 -f Dockerfile.base . && \
-docker buildx build --platform linux/arm64 --push -t ${REPO}:${VERSION}.base.arm64 -f Dockerfile.base . && \
-docker buildx build --platform linux/amd64 --push -t ${REPO}:${VERSION}.static.amd64 -f Dockerfile.static . && \
-docker buildx build --platform linux/arm64 --push -t ${REPO}:${VERSION}.static.arm64 -f Dockerfile.static . && \
-docker buildx build --platform linux/amd64 --push -t ${REPO}:${VERSION}.dynamic.amd64 -f Dockerfile.dynamic . && \
-docker buildx build --platform linux/arm64 --push -t ${REPO}:${VERSION}.dynamic.arm64 -f Dockerfile.dynamic .
+tags() {
+  TYPE=$1
+  TAGS="--tag ${REPO}:${TYPE}.${VERSION_FULL} \
+        --tag ${REPO}:${TYPE}.${MAJOR}.${MINOR}"
+  if [[ $TYPE == 'static' ]]; then
+    TAGS="${TAGS} --tag ${REPO}:latest --tag ${REPO}:${VERSION_FULL} --tag ${REPO}:${MAJOR}.${MINOR}"
+  fi
+  echo "${TAGS}"
+}
+
+echo ========== Building base image  ==========
+docker buildx build --platform linux/amd64,linux/arm64 --push $(labels) $(tags base) -f Dockerfile.base .
+echo ========== Building dynamic image  ==========
+docker buildx build --platform linux/amd64,linux/arm64 --push $(labels) $(tags dynamic) -f Dockerfile.dynamic .
+echo ========== Building static image  ==========
+docker buildx build --platform linux/amd64,linux/arm64 --push $(labels) $(tags static) -f Dockerfile.static .
+echo ========== Done  ==========
