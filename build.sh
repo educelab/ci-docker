f79853dc9b11d10d2793242776a1624d95919517
Seth Parker <csparker247@gmail.com>
2025-09-29 12:02:45 -0400

    Remove entrypoint

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ timestamp() {
 REPO=ghcr.io/educelab/ci-docker
 VER_MAJOR=12
 VER_MINOR=1
-VER_PATCH=1
+VER_PATCH=2
 VER_EXTRA=
 VER_FULL=${VER_MAJOR}.${VER_MINOR}.${VER_PATCH}${VER_EXTRA}
 REV=$(git rev-parse --verify HEAD)
